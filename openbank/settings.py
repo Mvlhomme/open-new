67709ca16154fb2649416c69d30b06caaf7a8a4b
Malhomme <farrellndv770@gmail.com>
2023-01-02 18:44:55 +0100

    first commit

--- openbank/settings.py
+++ openbank/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kk(pbb$-eeyf@w(5$bfwf69&-hozr&-q3e5lfgcg=t0pp20epk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
